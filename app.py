132a9d5be43de04b25d5ce361d5fd29cb47ec8ba
gauravsharrma <gauravsharrma00@gmail.com>
2025-04-11 22:13:36 +0400

    Update app.py

--- app.py
+++ app.py
@@ -37,3 +37,8 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True, host='0.0.0.0', port=port)
+
+@app.route('/debug/show-notes')
+def show_notes():
+    all_notes = Note.query.all()
+    return '<br>'.join(f"- {note.text}" for note in all_notes)
